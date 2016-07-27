0402ce760457dfe210c11dc5706930b24bd21fbc
Dan McCoy <dan.mccoy@mccoysoftware.uk>
2016-07-27 20:07:53 +0100

    Added basic IP, login and password prompts.

--- RaspbianQuickConfig.ps1
+++ RaspbianQuickConfig.ps1
@@ -2,3 +2,15 @@
 Write-Output "---Raspbian Quick Config Script---"
 Write-Output "----------------------------------"
 Write-Output ""
+
+#variables
+$thisDir = Split-Path -Path $MyInvocation.MyCommand.Definition -Parent
+$ipAddress = ""
+$login = ""
+$password = ""
+
+$ipAddress = Read-Host -Prompt "IP address"
+$Login = Read-Host -Prompt "Login"
+
+$response = Read-host "Password" -AsSecureString
+$password = [Runtime.InteropServices.Marshal]::PtrToStringAuto([Runtime.InteropServices.Marshal]::SecureStringToBSTR($response))
