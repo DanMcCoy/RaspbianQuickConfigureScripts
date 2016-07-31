1ea8d21c35fe314791b4ef0b7406adc6e4fa0e1d
Dan McCoy <dan.mccoy@mccoysoftware.uk>
2016-07-31 21:05:05 +0100

    Script now sets a static IP

--- Network/SetStaticIP.ps1
+++ Network/SetStaticIP.ps1
@@ -32,7 +32,19 @@ if ($(PromptForSetStaticIP($login)) -eq $TRUE)
     (Get-Content "$thisDir\Network\interfaces").replace('gateway [[gateway]]', "gateway $gateway") | Set-Content "$thisDir\Network\interfaces"
     (Get-Content "$thisDir\Network\interfaces").replace('network [[network]]', "network $network") | Set-Content "$thisDir\Network\interfaces"
     (Get-Content "$thisDir\Network\interfaces").replace('broadcast [[broadcast]]', "broadcast $broadcast") | Set-Content "$thisDir\Network\interfaces"
+    Write-Output ""
 
-    ./pscp -pw $password $thisDir\Network\interfaces $login@$($ipAddress):/etc/network
+    #copy the file over to the target machine
+    Write-Output "Copying new interfaces file to $($ipAddress):/home/$login"
+    ./pscp -pw $password $thisDir\Network\interfaces $login@$($ipAddress):/home/pi
+    Write-Output ""
+
+    #move it to the correct place
+    $moveCmd = "sudo mv -f -v /home/$login/interfaces /etc/network/interfaces"
+    ./plink.exe -ssh $ipAddress -l $login -pw $password $moveCmd  > "$thisDir\output.txt"
+
+    
+    Get-Content "$thisDir\output.txt"
+    Write-Output ""
 
 }
